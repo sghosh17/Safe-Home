9d2609b025625c42c3b6c3bd82b6d7c10702fa3c
PraeDev <colinperrydeveloper@gmail.com>
2021-12-02 19:30:39 +0000

    added zoopla

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ Bon;
 var name = "Andre";
 var pets = 3;
 var isStudent = true;
+
+
+$.ajax({
+    type: "GET",
+    url: "https://api.charitycommission.gov.uk/register/api/searchCharityName/UNICEF",
+
+    // Request headers
+    beforeSend: function(xhrObj) {
+        xhrObj.setRequestHeader("Cache-Control", "no-cache");
+        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "a40347f62a444906bbf75fd5e75d2882");
+        },
+    })
+.done(function (data) {
+    alert("success");
+})
+.fail(function () {
+    alert("error");
+});
