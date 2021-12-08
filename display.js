fb5206878185733f8a18b26c48c763befebbe69e
Sampreeti Das <sampreetighosh@gmail.com>
2021-12-08 01:24:20 +0000

    modified display file

--- display.js
+++ display.js
@@ -14,6 +14,22 @@ if (localData) {
 var imageEl = document.createElement("img");
 imageEl.src = parsedLocalData.image_url;
 displayEl.appendChild(imageEl);
+displayEl.appendChild(document.createElement("br"));
+
+var titleEl = document.createElement("p");
+titleEl.innerText = parsedLocalData.title;
+displayEl.appendChild(titleEl);
+displayEl.appendChild(document.createElement("br"));
+
+var addressEl = document.createElement("p");
+addressEl.innerText = parsedLocalData.displayable_address;
+displayEl.appendChild(addressEl);
+displayEl.appendChild(document.createElement("br"));
+
+var descripttionEl = document.createElement("p");
+descripttionEl.innerText = parsedLocalData.short_description;
+displayEl.appendChild(descripttionEl);
+displayEl.appendChild(document.createElement("br"));
 
 var detailurlEl = document.createElement("a");
 detailurlEl.href = parsedLocalData.details_url;
